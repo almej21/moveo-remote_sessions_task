3198fac56324a9e005416dd894c63af3d11d720e
almej21 <almogaharon21@gmail.com>
2023-11-01 12:37:07 +0200

    first commit, navbar with two pages, lobby and code

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import StateProvider from "context/StateProvider.js";
+import CodePage from "pages/code/CodePage";
+import LobbyPage from "pages/lobby/LobbyPage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <StateProvider>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/lobby " element={<LobbyPage />} />
+            <Route path="/code" element={<CodePage />} />
+          </Routes>
+        </div>
+      </StateProvider>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+// its critical to use the is_logged_in value from the local storage only
+//for rendering data that is not private.
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">ALMOG</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <li>
+          <Link className="link" to="/lobby">
+            LOBBY
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/code">
+            CODE
+          </Link>
+        </li>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
