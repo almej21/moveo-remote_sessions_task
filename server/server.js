f83ba292ff1d277ece56f40026dac1191ee1a393
almej21 <almogaharon21@gmail.com>
2023-11-04 18:25:16 +0200

    fixes for deployment

--- server/server.js
+++ server/server.js
@@ -61,21 +61,10 @@ app.use("/codeblock", codeBlockRouter);
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(
-    express.static(
-      path.join(__dirname1.substring(0, __dirname1.length - 7), "/client/build")
-    )
-  );
+  app.use(express.static(path.join(__dirname1, "/client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(
-        __dirname1.substring(0, __dirname1.length - 7),
-        "client",
-        "build",
-        "index.html"
-      )
-    );
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
